4a3b8f6bd59600ccd668bb72de8847b49159a091
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-12-18 01:36:52 -0500

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express');
 const app = express();
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
   res.render('home')
 })
 
-// GET  /places
-app.get('/', (req, res) => {
-  let places= []
-  res.render('places/index')
-})
-
 app.get('*', (req, res) => {
   res.render('error404')
 })
+
+// // GET  /places
+// app.get('/', (req, res) => {
+//   let places= []
+//   res.render('places/index')
+// })
+
+
   
 app.listen((process.env.PORT),(req, res) => {
   console.log(`App listening on port ${process.env.PORT}!`)
--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,9 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                {/* BOOTSTRAP CDN LINK (long) */}
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />                
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
                 {html.children}
