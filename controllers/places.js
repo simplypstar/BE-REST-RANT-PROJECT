5dfc828fb29233cb3f0a98d1f36ce1b50770f229
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-12-20 22:48:35 -0500

    Added show page route

--- controllers/places.js
+++ controllers/places.js
@@ -11,6 +11,24 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+// GET a Place by its id number
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
+
+
+// POST new Place
 router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
